11c25f9a23ae3e54c13e523bd5a0b682cbc3ab86
SaidARSLAN <lost.myreligion@hotmail.com>
2023-09-28 16:06:14 +0300

    Added dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import ItemList from './components/ItemList';
 import AddItem from './components/AddItem';
 import { Provider } from './context/MainContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faMinus, faMoon, faPlus, faSun } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
 
   const [toggle, setToggle] = useState(true);
+  const [darkToggle,setDarkToggle] = useState(false);
   const openCreateMenu = () => {
     setToggle(!toggle);
     const addItem = document.querySelector("#add-item");
@@ -22,14 +23,27 @@ function App() {
     }
     
   }
-
+  const handleDark = () => {
+    if (!darkToggle) {
+      document.body.classList.add("dark");
+      setDarkToggle(true);
+    }
+    else {
+      document.body.classList.remove("dark");
+      setDarkToggle(false);
+    }
+  }
+ 
   return (
     <Provider>
-    <main className='px-24 py-12'>
-      <div className='flex w-full items-center justify-end pb-4 lg:justify-start' onClick={openCreateMenu}>
-            <h1 className='text-2xl bg-gray-800 text-white px-4 py-1 cursor-pointer hover:bg-black hover:text-white duration-200'>
+    <main className='px-24 py-12 dark:bg-black' id='main'>
+      <div className='flex w-full items-center justify-between pb-4' >
+            <h1 className='text-2xl bg-gray-800 text-white px-4 py-1 cursor-pointer hover:bg-black hover:text-white duration-200' onClick={openCreateMenu}>
               {toggle ? <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon> : <FontAwesomeIcon icon={faMinus}></FontAwesomeIcon>}
               </h1>
+              <div onClick={handleDark}>
+              {darkToggle ? <FontAwesomeIcon icon={faSun} className='text-black bg-white text-2xl cursor-pointer hover:bg-black hover:text-white px-4 py-2 duration-200'></FontAwesomeIcon>:<FontAwesomeIcon icon={faMoon} className='text-2xl cursor-pointer hover:bg-black hover:text-white px-4 py-2 duration-200'></FontAwesomeIcon>}
+              </div>
       </div>
       <AddItem />
       <ItemList/>
--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -14,6 +14,8 @@ const ItemCard = ({item}) => {
     const [edittedImg, setEdittedImg] = useState(item.image);
     const [toggle,setToggle] = useState(true);
     const {sendDeletedItem,sendEditedItem} = useContext(GlobalContext)
+
+
     const handleOpen = () => {
         setToggle(!toggle);
         if (toggle) {
@@ -53,7 +55,8 @@ const ItemCard = ({item}) => {
     }
     return (
     <div>
-        {editControl ?  <form className='flex flex-col space-y-4 w-full'>
+        {editControl ?
+        <form className='flex flex-col space-y-4 w-full'>
             <input className='border-2 px-4 py-2 font-roboto' placeholder='Product Name' value={edittedName} onChange={e => setEdittedName(e.target.value)}></input>
             <textarea className='border-2 px-4 p-2 font-roboto' rows={5} placeholder='Product Description' value={edittedExplanation} onChange={e => setEdittedExplanation(e.target.value)}></textarea>
             <select className='border-2 font-roboto px-2 py-2' value={edittedCategory} onChange={e => setEditteCategory(e.target.value)}>
@@ -68,10 +71,11 @@ const ItemCard = ({item}) => {
             <input className='border-2 w-1/3 px-4 py-2' placeholder='$' value={edittedPrice} onChange={e => setEdittedPrice(e.target.value)}></input>
             <button className='bg-green-600 text-white px-6 text-lg py-2 font-roboto' onClick={handleSuccessEdit}>Edit</button>
             </div>
-        </form>:
-        <div className='flex flex-col items-center justify-center w-full text-center border-2 shadow-2xl py-4 lg:text-left'>
+        </form>
+        :
+        <div className='flex flex-col items-center justify-center w-full text-center border-2 shadow-2xl dark:shadow-slate-600 dark:shadow-md py-4 lg:text-left'>
         <div className='flex flex-col w-full items-end'>
-            <div onClick={handleOpen}>{toggle ? <FontAwesomeIcon icon={faSortDown} className='px-4 text-2xl cursor-pointer'></FontAwesomeIcon> : <FontAwesomeIcon icon={faSortUp} className='px-4 text-2xl cursor-pointer'></FontAwesomeIcon>}</div>
+            <div onClick={handleOpen}>{toggle ? <FontAwesomeIcon icon={faSortDown} className='px-4 text-2xl cursor-pointer dark:text-white'></FontAwesomeIcon> : <FontAwesomeIcon icon={faSortUp} className='px-4 text-2xl cursor-pointer dark:text-white'></FontAwesomeIcon>}</div>
             <div className='flex w-full justify-end px-4 py-2 space-x-2 max-h-0 overflow-hidden duration-200' ref={section}>
             <button className='bg-purple-400  text-white px-2' onClick={handleEdit}><FontAwesomeIcon icon={faEdit}></FontAwesomeIcon></button>
             <button className='bg-red-600 text-white px-2' onClick={handleDelete}><FontAwesomeIcon icon={faRemove}></FontAwesomeIcon></button>
@@ -79,8 +83,8 @@ const ItemCard = ({item}) => {
         </div>
         <img src={item.image} className='w-40'/>
         <div className='flex flex-col items-center justify-center space-y-4 px-4 py-4 lg:items-start'>
-            <h3 className='font-roboto text-xl'>{item.title}</h3>
-            <p className='font-sans text-sm'>{item.description}</p>
+            <h3 className='font-roboto text-xl dark:text-white'>{item.title}</h3>
+            <p className='font-sans text-sm dark:text-white'>{item.description}</p>
             <p className='font-roboto bg-gray-200 px-2 py-2'>{item.category}</p>
             <button className='bg-gray-900 text-white font-roboto px-4 py-2 text-sm hover:bg-black hover:text-white duration-200'>Add {item.price}$</button>
         </div> 
