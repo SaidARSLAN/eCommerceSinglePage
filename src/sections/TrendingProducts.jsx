9df11a472b78ad567d282787f743fa6b03f392fd
SaidARSLAN <lost.myreligion@hotmail.com>
2023-11-03 15:39:24 +0300

    Added usestates

--- src/sections/TrendingProducts.jsx
+++ src/sections/TrendingProducts.jsx
@@ -1,51 +1,45 @@
 import React, { useEffect, useReducer, useState } from 'react'
 import {BiRightArrow} from 'react-icons/bi';
 import {BiLeftArrow} from 'react-icons/bi'
-import products, { fetchProducts } from '../store/products'
-import {useDispatch,useSelector} from 'react-redux'
-import Product from '../components/Product';
+import {useSelector} from 'react-redux'
 
-const initialState = {
-    from : 0,
-    to : 1
-}
 
-const reducer = (state,action) => {
-    switch(action) {
-        case "BACK":
-            if (state.from > 0) {
-                state.from -= 1
-                state.to -= 1
-            }
-            return state
-        case "NEXT":
-            if (state.to < 8){
-                state.from += 1
-                state.to += 1
-            }
-            
-            return state
-        default:
-            console.log("ERROR!")
-    }
-}
+
+
 
 const TrendingProducts = () => {
 
-    const products = useSelector((state) => state.products.products)
-    const [product,dispatch] = useReducer(reducer,initialState)
-    console.log(product);
+    const products = useSelector((state) => state.products.products);
+    const [step, setStep] = useState(0)
+    const [trends, setTrends] = useState(products.slice(step,step+2))
+    
+    const handleBack = () => {
+        if (step > 0 ) {
+            setStep((currStep) => currStep - 1)
+        }
+    }
+
+    const handleNext = () => {
+        if (step < products.length - 1) {
+            setStep((currStep) => currStep + 1);
+        }
+    }
+    useEffect(() => {
+        setTrends(products.slice(step,step+2));
+    },[step])
   return (
     <section className='trending-products'>
         <div className='trending-header'>
             <h1>Trending Now</h1>
             <div className='buttons'>
-                <button  onClick={() => dispatch("BACK")}><BiLeftArrow color='white' size={25}/></button>
-                <button onClick={() => dispatch("NEXT")}><BiRightArrow color='white' size={25} /></button>
+                <button onClick={handleBack}><BiLeftArrow color='white' size={25}/></button>
+                <button onClick={handleNext}><BiRightArrow color='white' size={25}/></button>
             </div>
         </div>
         <div className='trending-list'>
-            
+            {trends.map((trend) => {
+                return <p>{trend.title}</p>
+            })}
         </div>
     </section>
   )
