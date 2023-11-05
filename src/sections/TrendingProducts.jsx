35be8dbe496421db2018cecedc4861237c736a2d
SaidARSLAN <lost.myreligion@hotmail.com>
2023-11-05 18:22:13 +0300

    Trending page has been fixed

--- src/sections/TrendingProducts.jsx
+++ src/sections/TrendingProducts.jsx
@@ -2,31 +2,42 @@ import React, { useEffect, useReducer, useState } from 'react'
 import {BiRightArrow} from 'react-icons/bi';
 import {BiLeftArrow} from 'react-icons/bi'
 import {useSelector} from 'react-redux'
-import Product from '../components/Product';
+
 
 
 const TrendingProducts = () => {
 
-    const products = useSelector((state) => state.products.products);
-    const [step, setStep] = useState(0)
-    const [trends, setTrends] = useState([])
-    useEffect(() => {
-        setTrends(products.slice(step,step+2));
-    },[step,products])
-
-
-    const handleBack = () => {
-        if (step > 0 ) {
-            setStep((currStep) => currStep - 1)
-        }
+  const products = useSelector((state) => state.products.products)
+  const [step, setStep] = useState(0);
+  const [firstProducts, setFirstProducts] = useState([]);
+  const [currentWidth, setCurrentWidth] = useState(window.innerWidth);
+  useEffect(() => {
+      const handleResize  = () => {
+        setCurrentWidth(window.innerWidth);
+      }
+      window.addEventListener('resize',handleResize);
+      return () => {
+        window.removeEventListener('resize',handleResize)
+      }
+  },[])
+  const handleNext = () => {
+    if (step < products.length  - firstProducts.length) {
+      setStep((step) => step += 1); 
     }
-
-    const handleNext = () => {
-        if (step < products.length - 2) {
-            setStep((currStep) => currStep + 1);
-        }
+  }
+  const handleBack = () => {
+    if (step > 0) {
+      setStep((step) => step -= 1);
     }
-    
+  }
+  useEffect(() => {
+    if (currentWidth > 1000) {
+      setFirstProducts(products.slice(0+step,5+step))
+    }
+    else {
+      setFirstProducts(products.slice(0+step,2+step))
+    }
+  },[step,products,currentWidth])
   return (
     <section className='trending-products'>
         <div className='trending-header'>
@@ -36,11 +47,14 @@ const TrendingProducts = () => {
                 <button onClick={handleNext}><BiRightArrow color='white' size={25}/></button>
             </div>
         </div>
-        <motion className='trending-list'>
-            {trends.map((trend) => {
-                return <Product product={trend}/>
-            })}
-        </motion>
+        <div className='trending-body'>
+          {firstProducts.map((product) => {
+            return <div>
+              <img src={product.image} />
+              <p>{product.title}</p>
+              </div>
+          })}
+        </div>
     </section>
   )
 }
