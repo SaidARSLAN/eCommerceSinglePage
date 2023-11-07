67d153d44ac47c6a699db082395cb11a98a0c91f
SaidARSLAN <lost.myreligion@hotmail.com>
2023-11-08 00:16:23 +0300

    Added quantity section

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -4,6 +4,7 @@ import products, { fetchProducts } from '../store/products';
 import { useDispatch, useSelector } from 'react-redux';
 
 const ProductDetail = () => {
+  const [quantity, setQuantity] = useState(0);
     const [product, setProduct] = useState("")
     const products = useSelector(state => state.products.products);
     const {url} = useParams();
@@ -17,13 +18,29 @@ const ProductDetail = () => {
         console.log(product)
     },[products,url])
 
+    const handleDecrease = () => {
+      if (quantity > 0) {
+        setQuantity((currQuantity) => currQuantity - 1)
+      } 
+    }
+    const handleIncrease = () => {
+      setQuantity((currQuantity) => currQuantity + 1)
+    }
+
   return (
       <div className='product-detail'>
         <img src={product && product.image} />
         <h1>{product && product.title.split(" ").slice(0,4).join(" ")}</h1>
         <div className='product-buy'>
         <p>{product && product.description}</p>
-        <p>{product && product.price}</p>
+        <div className='product-price'>
+          <p>Quantity : {quantity}</p>
+          <div className='buttons'>
+            <button onClick={handleDecrease}>-</button>
+            <button onClick={handleIncrease}>+</button>
+          </div>
+        <p>{product && (quantity* product.price).toString().slice(0,5)} $</p>
+        </div>
         </div>
     </div>
   )
